3702566f71bdb6886cf3a9ad3f552e02556f9d35
Hobart2967 <hobart2967@outlook.com>
2026-07-26 22:47:15 +0200

    feat(ntfy): :sparkles: Add ntfy with user setup and permissions

---
+++ k8s/system/sys-ntfy/users.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <namespace>"
+  exit 1
+fi
+
+echo "Extracting consumer values from secrets in namespace $1"
+
+mkdir -p .tmp
+
+YQ_VERSION=$(yq --version 2>/dev/null || true)
+if echo "$YQ_VERSION" | grep -qi "mikefarah"; then
+  CONFIGMAPS=$(kubectl -n "$1" get secrets -o yaml | yq eval '.items
+    | map(select(.metadata.annotations."hobart2967.github.io/ntfy-user" == "true"))
+    | map({"name": (.data.username | @base64d), "password": .data.password, "role": (.data.role | @base64d), "extras": (.data.extras)})' -)
+else
+  echo "This script requires mikefarah yq for parsing secret YAML strings"
+  exit 1
+fi
+
+#echo $CONFIGMAPS
+echo "$CONFIGMAPS" > .tmp/users.yaml
